b56c045da8af1b88ff98fb2e25c2309159e9f345
venla <venla.e.rantanen@gmail.com>
2023-04-04 10:30:30 +0300

    add view for listing all projects, display view on start

---
+++ src/ui/project_view.py
@@ -0,0 +1,72 @@
+from tkinter import ttk, constants
+from services.project_service import project_service
+
+class ProjectListView:
+    def __init__(self, root, projects):
+        self._root = root
+        self._projects = projects
+        self._frame = None
+        self._initialize()
+        
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+        
+    def destroy(self):
+        self._frame.destroy()
+            
+    def _initialize_project(self, project):
+        project_frame = ttk.Frame(master=self._frame)
+                
+        project_created_at = ttk.Label(master=project_frame, text=project.created_at)
+        project_created_at.config(font=("Arial", 8))
+        project_created_at.grid(row=0, column=0)
+        
+        project_title = ttk.Label(master=project_frame, text=project.title) 
+        project_title.grid(row=1, column=0)
+        
+        project_frame.config(padding=16, border=2)
+        project_frame.pack()
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+        title = ttk.Label(master=self._frame, text="Projects")
+        title.config(font=("Arial", 16), padding=8)
+        title.pack(fill=constants.X)
+
+        for project in self._projects:
+            self._initialize_project(project)
+        
+
+class ProjectView:
+    def __init__(self, root):
+        self._root = root
+        self._frame = None
+        self._project_list_view = None
+        self._project_list_frame = None
+        self._initialize()
+        
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+        
+    def destroy(self):
+        self._frame.destroy()
+        
+    def initialize_project_list(self):
+        if self._project_list_view:
+            self._project_list_view.destroy()
+            
+        projects = project_service.get_all_projects()
+        self._project_list_view = ProjectListView(self._project_list_frame, projects)
+        self._project_list_view.pack()
+        
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+        self._project_list_frame = ttk.Frame(master=self._frame)
+        self.initialize_project_list()
+        self._project_list_frame.grid(
+            row=0,
+            column=0,
+        )
+        
+
+        
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,10 +1,22 @@
 from tkinter import ttk
+from ui.project_view import ProjectView
 
 class UI:
     def __init__(self, root):
         self._root = root
+        self._current_view = None
 
     def start(self):
         self._root.configure(background="grey")
         self._root.minsize(500, 500) 
+        self._show_project_view()
         
+    def _destroy_view(self):
+        if self._current_view:
+            self._current_view.destroy()
+        self._current_view = None
+    
+    def _show_project_view(self):
+        self._destroy_view()
+        self._current_view = ProjectView(self._root)
+        self._current_view.pack()
